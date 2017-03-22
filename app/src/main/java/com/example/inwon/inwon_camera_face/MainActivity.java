3a2d228702ea209046fefffc7a4ab85d60e14fc8
naiw0001 <idc06096@naver.com>
2017-03-22 18:03:02 +0900

    Initial commit

---
+++ app/src/main/java/com/example/inwon/inwon_camera_face/MainActivity.java
@@ -0,0 +1,243 @@
+package com.example.inwon.inwon_camera_face;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private Mat img_input;
+    private Mat img_result;
+    private static final String tag= "opencv";
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    public static native long loadCascade(String cascadeFileName);
+    public static native void detect(long cascadeClassifier_face, long cascadeClassifier_eye, long matAddrInput, long matAddrResult);
+    public long cascadeClassifier_face = 0;
+    public long cascadeClassifier_eye = 0;
+
+    static final int PERMISSION_REQUEST_CODE = 1;
+    String[] PERMISSIONS  = {"android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    static {
+        System.loadLibrary("opencv_java3");
+        System.loadLibrary("native-lib");
+    }
+
+    private boolean hasPermissions(String[] permissions) {
+        int ret = 0;
+        //스트링 배열에 있는 퍼미션들의 허가 상태 여부 확인
+        for (String perms : permissions) {
+            ret = checkCallingOrSelfPermission(perms);
+            if (!(ret == PackageManager.PERMISSION_GRANTED)) {
+                //퍼미션 허가 안된 경우
+                return false;
+            }
+
+        }
+        //모든 퍼미션이 허가된 경우
+        return true;
+    }
+    private void requestNecessaryPermissions(String[] permissions) {
+        //마시멜로( API 23 )이상에서 런타임 퍼미션(Runtime Permission) 요청
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void copyFile(String filename) {
+        String baseDir = Environment.getExternalStorageDirectory().getPath();
+        //String baseDir = getFilesDir().getPath();
+        String pathDir = baseDir + File.separator + filename;
+
+
+        AssetManager assetManager = this.getAssets();
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        try {
+            Log.d( TAG, "copyFile :: 다음 경로로 파일복사 "+ pathDir);
+            inputStream = assetManager.open(filename);
+            outputStream = new FileOutputStream(pathDir);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+            inputStream.close();
+            inputStream = null;
+            outputStream.flush();
+            outputStream.close();
+            outputStream = null;
+        } catch (Exception e) {
+            Log.d(TAG, "copyFile :: 파일 복사 중 예외 발생 "+e.toString() );
+        }
+
+    }
+
+    private void read_cascade_file(){
+        copyFile("haarcascade_frontalface_alt.xml");
+        copyFile("haarcascade_eye_tree_eyeglasses.xml");
+
+        cascadeClassifier_face = loadCascade( "haarcascade_frontalface_alt.xml");
+        cascadeClassifier_eye = loadCascade( "haarcascade_eye_tree_eyeglasses.xml");
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    mOpenCvCameraView.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
+        switch(permsRequestCode){
+
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean camreaAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                        if (!camreaAccepted || !writeAccepted )
+                        {
+                            showDialogforPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+                            return;
+                        }else
+                        {
+                            read_cascade_file();
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showDialogforPermission(String msg) {
+
+        final AlertDialog.Builder myDialog = new AlertDialog.Builder(  MainActivity.this);
+        myDialog.setTitle("알림");
+        myDialog.setMessage(msg);
+        myDialog.setCancelable(false);
+        myDialog.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(PERMISSIONS, PERMISSION_REQUEST_CODE);
+                }
+
+            }
+        });
+        myDialog.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+        myDialog.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if(!hasPermissions(PERMISSIONS)){ // 퍼미션 허가 여부 확인
+            requestNecessaryPermissions(PERMISSIONS);
+        }else {
+            //이미 허가 받은 상태
+            read_cascade_file();
+        }
+        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.setCameraIndex(1); // front-camera = 1
+        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        img_input = inputFrame.rgba();
+        Core.flip(img_input,img_input,1);
+        img_result = new Mat();
+
+        detect(cascadeClassifier_face,cascadeClassifier_eye,img_input.getNativeObjAddr(),img_result.getNativeObjAddr());
+
+        return img_result;
+    }
+}
